0bfb70a6e5964c74e11bc03d8983bae491bfe937
Maaria Tiiri <maaria.tiiri@aalto.fi>
2026-07-27 21:42:45 +0300

    4.14 done

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -20,4 +20,39 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const blog = await Blog.findByIdAndDelete(request.params.id)
+  if (blog) {
+    response.status(204).end()
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+
+  const body = request.body
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+        return response.status(404).end()
+    }
+  blog.title = body.title
+  blog.author = body.author
+  blog.url = body.url
+  blog.likes = body.likes
+  const updatedBlog = await blog.save()
+  console.log(updatedBlog)
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -1,5 +1,5 @@
 const assert = require('node:assert')
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -24,71 +24,199 @@ test('blogs have an unique "id" component', async () => {
   assert(response.body.every(b => b.id !== null))
 })
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'What do Dark Souls games and studying physics have in common?',
-    author: 'Maaria Tiiri',
-    url: 'fyysikkokilta.fi',
-    likes: '1'
-  }
+describe('adding a new blog', ()=> {
+    
+    test('a valid blog can be added ', async () => {
+        const newBlog = {
+            title: 'What do Dark Souls games and studying physics have in common?',
+            author: 'Maaria Tiiri',
+            url: 'fyysikkokilta.fi',
+            likes: '1'
+        }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb()
+        const blogsAtEnd = await helper.blogsInDb()
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-  const titles = blogsAtEnd.map(b => b.title)
-  assert(titles.includes('What do Dark Souls games and studying physics have in common?'))
+        const titles = blogsAtEnd.map(b => b.title)
+        assert(titles.includes('What do Dark Souls games and studying physics have in common?'))
+    })
+
+    test('likes are set to zero if they are missing', async () => {
+        const newBlog = {
+            title: 'Why Elden Ring is better without summons',
+            author: 'Maaria Tiiri',
+            url: 'fyysikkokilta.fi',
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDb()
+
+        const likelessBlog = blogsAtEnd.filter(b => b.title === 'Why Elden Ring is better without summons')[0]
+        assert.strictEqual(likelessBlog.likes, 0)
+    })
+
+    test('missing title or url results in 400 bad request', async () => {
+        const blogWithoutTitle = {
+            author: 'Maaria Tiiri',
+            url: 'fyysikkokilta.fi',
+        }
+        const blogWithoutUrl = {
+            title: 'Shouting into the void',
+            author: 'Maaria Tiiri'
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(blogWithoutTitle)
+            .expect(400)
+
+        await api
+            .post('/api/blogs')
+            .send(blogWithoutUrl)
+            .expect(400)
+
+        const blogsAtEnd = await helper.blogsInDb()
+
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    })
 })
 
-test.only('likes are set to zero if they are missing', async () => {
-  const newBlog = {
-    title: 'Why Elden Ring is better without summons',
-    author: 'Maaria Tiiri',
-    url: 'fyysikkokilta.fi',
-  }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb()
+describe('viewing a specific blog', ()=> {
+    test('a valid blog can be viewed', async () => {
+        const initiallyFoundBlogs = await helper.blogsInDb()
+        
+        const blogToView = initiallyFoundBlogs[0]
 
-  const likelessBlog = blogsAtEnd.filter(b => b.title === 'Why Elden Ring is better without summons')[0]
-  assert.strictEqual(likelessBlog.likes, 0)
+        const resultBlog = await api
+            .get(`/api/blogs/${blogToView.id}`)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        assert.deepStrictEqual(resultBlog.body, blogToView)
+        })
+
+    test('fails with statuscode 404 if note does not exist', async () => {
+        const validNonexistingId = await helper.nonExistingId()
+
+        await api.get(`/api/blogs/${validNonexistingId}`).expect(404)
+    })
+
+    test('fails with statuscode 400 id is invalid', async () => {
+        const invalidId = '5a3d5da59070081a82a3445'
+
+        await api.get(`/api/blogs/${invalidId}`).expect(400)
+    })
 })
 
-test.only('missing title or url results in 400 bad request', async () => {
-  const blogWithoutTitle = {
-    author: 'Maaria Tiiri',
-    url: 'fyysikkokilta.fi',
-  }
-  const blogWithoutUrl = {
-    title: 'Shouting into the void',
-    author: 'Maaria Tiiri'
-  }
+describe('deleting a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const initiallyFoundBlogs = await helper.blogsInDb()
+      const blogToDelete = initiallyFoundBlogs[0]
 
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutTitle)
-    .expect(400)
+      await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
 
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutUrl)
-    .expect(400)
+      const blogsAtEnd = await helper.blogsInDb()
 
-  const blogsAtEnd = await helper.blogsInDb()
+      const ids = blogsAtEnd.map(n => n.id)
+      assert(!ids.includes(blogToDelete.id))
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+    })
+
+    test('fails with statuscode 400 id is invalid', async () => {
+        const invalidId = '5a3d5da59070081a82a3445'
+
+        await api.delete(`/api/blogs/${invalidId}`).expect(400)
+    })
+
+    test('fails with statuscode 404 if note is already deleted', async () => {
+        const validNonexistingId = await helper.nonExistingId()
+
+        await api.delete(`/api/blogs/${validNonexistingId}`).expect(404)
+    })
 })
+
+describe('updating blog info', ()=> {
+    test.only('updating likes succeeds if id is valid', async () => {
+        const newLikes = 100000
+
+        const initiallyFoundBlogs = await helper.blogsInDb()
+        const blogToUpdate = initiallyFoundBlogs[0]
+
+        const updatedBlog = await api
+            .put(`/api/blogs/${blogToUpdate.id}`)
+            .send({
+                title: blogToUpdate.title,
+                author: blogToUpdate.author,
+                url: blogToUpdate.url,
+                likes: newLikes
+            })
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        assert.strictEqual(updatedBlog.body.likes, newLikes)
+    })
+
+    test.only('fails with statuscode 400 if new value is invalid', async () => {
+        const newLikes = "many"
+
+        const initiallyFoundBlogs = await helper.blogsInDb()
+        const blogToUpdate = initiallyFoundBlogs[0]
+
+        const updatedBlog = await api
+            .put(`/api/blogs/${blogToUpdate.id}`)
+            .send({
+                title: blogToUpdate.title,
+                author: blogToUpdate.author,
+                url: blogToUpdate.url,
+                likes: newLikes
+            })
+            .expect(400)
+    })
+
+    test.only('fails with statuscode 400 if id is invalid', async () => {
+        const invalidId = '5a3d5da59070081a82a3445'
+
+        const updatedBlog = await api
+            .put(`/api/blogs/${invalidId}`)
+            .send({
+                title: "none",
+                author: "nobody",
+                url: "404.com",
+                likes: 0
+            })
+            .expect(400)
+    })
+
+    test.only('fails with statuscode 404 if blog is deleted', async () => {
+        const validNonexistingId = await helper.nonExistingId()
+
+        const updatedBlog = await api
+            .put(`/api/blogs/${validNonexistingId}`)
+            .send({
+                title: "none",
+                author: "nobody",
+                url: "404.com",
+                likes: 0
+            })
+            .expect(404)
+    })
+})
+
+
 
 after(async () => {
   await mongoose.connection.close()
