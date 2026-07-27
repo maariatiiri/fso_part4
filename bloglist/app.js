1a6094095028816606759c697519079e4148cf03
Maaria Tiiri <maaria.tiiri@aalto.fi>
2026-07-27 12:36:30 +0300

    4.2 done

---
+++ bloglist/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const Blog = require('./models/blog')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
