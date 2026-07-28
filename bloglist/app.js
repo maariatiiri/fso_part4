75f9d6b92a7ff802d3500798bf7927b0dc95ead8
Maaria Tiiri <maaria.tiiri@aalto.fi>
2026-07-28 14:09:01 +0300

    4.20 done

--- bloglist/app.js
+++ bloglist/app.js
@@ -13,7 +13,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use(express.json())
 app.use(middleware.requestLogger)
-
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
@@ -21,5 +21,6 @@ app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+
 
 module.exports = app
